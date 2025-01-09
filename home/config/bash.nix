0fb45be50e10cc73a32a31917545b2ce61f983e2
JaydenPahukula <jayden.pahukula@gmail.com>
2025-01-09 11:50:57 -0700

    update bash PS1

--- home/config/bash.nix
+++ home/config/bash.nix
@@ -2,9 +2,11 @@
 {...}: {
   programs.bash = {
     enable = true;
-    # custom prompt
-    bashrcExtra = ''
-      export PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] ";
+    bashrcExtra = let
+      # custom prompt
+      PS1 = ''\[\033[1;\$(if [ \$? == 0 ]; then echo 34; else echo 31; fi)m\]*\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] '';
+    in ''
+      export PS1="${PS1}";
 
       echo;
     '';
