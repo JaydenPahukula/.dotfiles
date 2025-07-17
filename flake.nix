5541ad9d852d0a9f18879e3ee577205a98eae51b
JaydenPahukula <jayden.pahukula@gmail.com>
2025-07-16 14:09:17 -1000

    switch to nixpkgs-25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Jayden's Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
