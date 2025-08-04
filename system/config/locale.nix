300d197b41cb9aa8b3c2d8e2145244b9c90e098d
JaydenPahukula <jayden.pahukula@gmail.com>
2025-08-03 21:13:38 -0500

    time zone change

--- system/config/locale.nix
+++ system/config/locale.nix
@@ -1,6 +1,5 @@
 # region and locale
 {...}: {
-  # time.timeZone = "America/Denver";
-  time.timeZone = "Pacific/Honolulu";
+  time.timeZone = "America/Chicago"; # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
   i18n.defaultLocale = "en_US.UTF-8";
 }
