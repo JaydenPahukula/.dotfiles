14d1d05f7d6ab4e5cbe861598b15e797cd8d7e60
JaydenPahukula <jayden.pahukula@gmail.com>
2025-09-05 23:34:40 -0500

    add home-manager package in system config

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,10 +1,15 @@
 # nixos modules
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./nix.nix
     ./system.nix
     ./utils.nix
     ./users.nix
+  ];
+
+  # install home-manager
+  environment.systemPackages = [
+    pkgs.home-manager
   ];
 
   system.stateVersion = "23.11"; # IMPORTANT: DO NOT CHANGE
