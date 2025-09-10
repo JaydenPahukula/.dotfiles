e33c5c6f173a87fd67c51c6ee26d6cfd9a36d1d3
JaydenPahukula <jayden.pahukula@gmail.com>
2025-09-09 20:07:19 -0500

    add waybar thermal zone option

--- modules/hm/programs/waybar/default.nix
+++ modules/hm/programs/waybar/default.nix
@@ -1,128 +1,147 @@
 # waybar config
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.programs.waybar.temp = {
+    thermal-zone = lib.mkOption {
+      type = lib.types.number;
+      default = null;
+      description = ''
+        The number of the thermal zone to use for the tempurature module.
+
+        Available thermal zones are listed in `/sys/class/thermal/`.
+      '';
+    };
+  };
+
   imports = [
     ./style.nix
   ];
 
-  home.packages = with pkgs; [
-    nerd-fonts.hack
-    # cava
-  ];
+  config = {
+    home.packages = with pkgs; [
+      nerd-fonts.hack
+      # cava
+    ];
 
-  # start on boot
-  programs.plasma.startup.startupScript."waybar" = {
-    runAlways = true;
-    text = "waybar &";
-  };
+    # start on boot
+    programs.plasma.startup.startupScript."waybar" = {
+      runAlways = true;
+      text = "waybar &";
+    };
 
-  # main config
-  programs.waybar = {
-    enable = true;
+    # main config
+    programs.waybar = {
+      enable = true;
 
-    settings."mainBar" = {
-      layer = "bottom"; # so yakuake goes above it
-      position = "top";
-      height = 24;
-      spacing = 20;
+      settings."mainBar" = {
+        layer = "bottom"; # so yakuake goes above it
+        position = "top";
+        height = 24;
+        spacing = 20;
 
-      modules-left = [
-        "custom/nixlogo"
-        "clock"
-      ];
-      modules-center = [];
-      modules-right = [
-        # "cava"
-        "idle_inhibitor"
-        "network"
-        "memory"
-        "cpu"
-        "temperature"
-        "battery"
-      ];
-
-      "custom/nixlogo" = {
-        format = "  ";
-        tooltip = false;
-        on-click = ". ${pkgs.writeShellScript "logout-prompt" ''
-          ${pkgs.kdePackages.qttools}/bin/qdbus org.kde.LogoutPrompt /LogoutPrompt promptAll
-        ''}";
-      };
-
-      clock = {
-        interval = 1;
-        format = "{:%I:%M %p %a, %b %d}";
-        tooltip-format = "<big>{:%I:%M:%S}</big>";
-      };
-
-      # cava = {
-      #   framerate = 60;
-      #   autosens = 1;
-      #   bars = 12;
-      #   method = "pulse";
-      #   source = "auto";
-      #   stereo = false;
-      #   bar_delimiter = 0;
-      #   noise_reduction = 0.2;
-      #   monstercat = false;
-      #   waves = false;
-      #   input_delay = 2;
-      #   format-icons = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
-      # };
-
-      idle_inhibitor = {
-        format = "{icon}";
-        format-icons = {
-          activated = "";
-          deactivated = "";
-        };
-        tooltip-format-activated = "Idle inhibitor: on";
-        tooltip-format-deactivated = "Idle inhibitor: off";
-      };
-
-      network = {
-        interval = 10;
-        format-wifi = "{essid} ({signalStrength}%) ";
-        format-ethernet = "{ipaddr}/{cidr} ";
-        format-linked = "{ifname} (No IP) ";
-        format-disconnected = "Disconnected ⚠";
-        tooltip-format = "IP = {ipaddr}/{cidr}\nOn {ifname} via {gwaddr}\n\n{bandwidthDownBits}   {bandwidthUpBits} ";
-      };
-
-      memory = {
-        interval = 3;
-        format = "Mem {}%";
-        tooltip-format = "{used:0.1f}GB out of {total:0.1f}GB";
-      };
-
-      cpu = {
-        interval = 3;
-        format = "CPU {usage}%";
-      };
-
-      temperature = {
-        interval = 3;
-        thermal-zone = 2;
-        critical-threshold = 60;
-      };
-
-      battery = {
-        interval = 10;
-        states = {
-          warning = 20;
-          critical = 10;
-        };
-        format = "{capacity}% {icon}";
-        format-full = "{capacity}% {icon}";
-        format-charging = "{capacity}% ";
-        format-plugged = "{capacity}% ";
-        format-tooltip = "Time to empty: {time}";
-        format-icons = [
-          ""
-          ""
-          ""
-          ""
-          ""
+        modules-left = [
+          "custom/nixlogo"
+          "clock"
         ];
+        modules-center = [];
+        modules-right = [
+          # "cava"
+          "idle_inhibitor"
+          "network"
+          "memory"
+          "cpu"
+          "temperature"
+          "battery"
+        ];
+
+        "custom/nixlogo" = {
+          format = "  ";
+          tooltip = false;
+          on-click = ". ${pkgs.writeShellScript "logout-prompt" ''
+            ${pkgs.kdePackages.qttools}/bin/qdbus org.kde.LogoutPrompt /LogoutPrompt promptAll
+          ''}";
+        };
+
+        clock = {
+          interval = 1;
+          format = "{:%I:%M %p %a, %b %d}";
+          tooltip-format = "<big>{:%I:%M:%S}</big>";
+        };
+
+        # cava = {
+        #   framerate = 60;
+        #   autosens = 1;
+        #   bars = 12;
+        #   method = "pulse";
+        #   source = "auto";
+        #   stereo = false;
+        #   bar_delimiter = 0;
+        #   noise_reduction = 0.2;
+        #   monstercat = false;
+        #   waves = false;
+        #   input_delay = 2;
+        #   format-icons = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
+        # };
+
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "";
+            deactivated = "";
+          };
+          tooltip-format-activated = "Idle inhibitor: on";
+          tooltip-format-deactivated = "Idle inhibitor: off";
+        };
+
+        network = {
+          interval = 10;
+          format-wifi = "{essid} ({signalStrength}%) ";
+          format-ethernet = "{ipaddr}/{cidr} ";
+          format-linked = "{ifname} (No IP) ";
+          format-disconnected = "Disconnected ⚠";
+          tooltip-format = "IP = {ipaddr}/{cidr}\nOn {ifname} via {gwaddr}\n\n{bandwidthDownBits}   {bandwidthUpBits} ";
+        };
+
+        memory = {
+          interval = 3;
+          format = "Mem {}%";
+          tooltip-format = "{used:0.1f}GB out of {total:0.1f}GB";
+        };
+
+        cpu = {
+          interval = 3;
+          format = "CPU {usage}%";
+        };
+
+        temperature = {
+          interval = 3;
+          thermal-zone = config.programs.waybar.temp.thermal-zone;
+          critical-threshold = 60;
+        };
+
+        battery = {
+          interval = 10;
+          states = {
+            warning = 20;
+            critical = 10;
+          };
+          format = "{capacity}% {icon}";
+          format-full = "{capacity}% {icon}";
+          format-charging = "{capacity}% ";
+          format-plugged = "{capacity}% ";
+          format-tooltip = "Time to empty: {time}";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
       };
     };
   };
