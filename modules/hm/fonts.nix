05702e426131f2428e6ac3aa29f3b913ce3fa0c7
JaydenPahukula <jayden.pahukula@gmail.com>
2025-09-15 21:02:18 -0500

    add fonts module and fix yakuake braille font

--- modules/hm/fonts.nix
+++ modules/hm/fonts.nix
@@ -1,6 +1,30 @@
 # fonts
-{...}: {
-  fonts.fontconfig.enable = true;
-  # global fonts
-  home.packages = [];
+{
+  config,
+  lib,
+  ...
+}: {
+  options.fonts.disabled = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    default = [];
+    example = ["Noto Sans"];
+    description = ''
+      A list of font families to disable on the system.
+    '';
+  };
+
+  config = {
+    xdg.configFile."fontconfig/conf.d/78-disable-fonts.conf".text = let
+      mkPattern = s: "\n      <pattern><patelt name=\"family\"><string>${s}</string></patelt></pattern>";
+    in ''
+      <?xml version="1.0"?>
+      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+      <fontconfig>
+        <selectfont>
+          <rejectfont>${lib.strings.concatMapStrings mkPattern config.fonts.disabled}
+          </rejectfont>
+        </selectfont>
+      </fontconfig>
+    '';
+  };
 }
