f02e0ae2d2113ea878aa4c9e70c16c8d6430eb38
JaydenPahukula <jayden.pahukula@gmail.com>
2025-10-03 22:57:37 -0500

    fix plasma panels

--- modules/hm/programs/plasma/panels.nix
+++ modules/hm/programs/plasma/panels.nix
@@ -1,6 +1,12 @@
 # plasma panels
-# NOTE: if panels don't update after reboot, try running ~/.local/share/plasma-manager/run_all.sh
 {...}: {
   # disable all panels
   programs.plasma.panels = [];
+  # delete all panels every time (not sure why this is necessary)
+  programs.plasma.startup.desktopScript."clear-panels" = {
+    runAlways = true;
+    text = ''
+      panels().forEach((panel) => panel.remove());
+    '';
+  };
 }
