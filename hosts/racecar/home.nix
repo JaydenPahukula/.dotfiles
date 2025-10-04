6cc015b6c98dd8985bc04aed4c959ecb4bc99a8a
JaydenPahukula <jayden.pahukula@gmail.com>
2025-10-03 22:21:40 -0500

    racecar: add wallpaper

--- hosts/racecar/home.nix
+++ hosts/racecar/home.nix
@@ -1,8 +1,14 @@
-{pkgs, ...}: {
+{
+  root,
+  pkgs,
+  ...
+}: {
   programs.waybar = {
     temp.thermal-zone = 1; # x86_pkg_temp
     gpu.hwmon-path = "/sys/class/hwmon/hwmon6/device/gpu_busy_percent";
   };
+
+  wallpaper = "${root}/assets/wallpapers/adriano.jpg";
 
   home.packages = with pkgs; [
     # apps
