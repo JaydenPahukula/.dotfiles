e74eb5222c19df795b5d33919414e1de002c3fa5
JaydenPahukula <jayden.pahukula@gmail.com>
2025-10-03 23:14:44 -0500

    remove duplicate config

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -55,8 +55,6 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
-  programs.git.enable = true;
-
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     oxygen
