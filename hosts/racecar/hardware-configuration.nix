ecdc66ed998c3e13898595b4811ff9c625cc0404
JaydenPahukula <jayden.pahukula@gmail.com>
2026-01-01 14:04:56 -0600

    racecar hardware changes

--- hosts/racecar/hardware-configuration.nix
+++ hosts/racecar/hardware-configuration.nix
@@ -8,25 +8,23 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a5d9942a-6d26-493c-a050-de972855e80d";
+    { device = "/dev/disk/by-uuid/7d97d210-aadb-4323-b7d5-dd38a731ffe0";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0200-9EDB";
+    { device = "/dev/disk/by-uuid/CC63-0E13";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/cf8c4c09-4029-4d47-bdaf-508992ffc836"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
