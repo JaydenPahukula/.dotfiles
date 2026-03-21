f057f251c5861ef640d445b1a2acbe71a391203f
JaydenPahukula <jayden.pahukula@gmail.com>
2026-03-20 20:21:58 -0500

    radar: basic network config

--- hosts/radar/configuration.nix
+++ hosts/radar/configuration.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, ...}: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -9,15 +9,22 @@
   in {
     time.timeZone = "America/Chicago";
 
+    networking.firewall = {
+      allowedUDPPorts = [53];
+      allowedTCPPorts = [53 80 443];
+    };
+
     # internal dns
     services.dnsmasq = {
       enable = true;
+      alwaysKeepRunning = true;
       settings = {
-        listen-address = ["::1" "127.0.0.1" router-ip]; # only listen to localhost and router
+        local-service = "net"; # only listen to local network
         server = ["1.1.1.1" "71.10.216.1" "71.10.216.2"]; # upstream dns servers
         address = [
           "/router/${router-ip}"
-          "/${config.services.grafana.settings.server.domain}/${radar-ip}"
+          "/radar/${radar-ip}"
+          "/.jaydenp.dev/${radar-ip}"
         ];
       };
     };
@@ -30,9 +37,39 @@
           http_port = 3000;
           enforce_domain = true;
           enable_gzip = true;
-          domain = "dashboard.radar";
+          domain = "grafana.jaydenp.dev";
         };
         analytics.reporting_enabled = false;
+      };
+    };
+
+    services.nginx = {
+      enable = true;
+      recommendedTlsSettings = true;
+      recommendedProxySettings = true;
+      virtualHosts = {
+        "grafana.jaydenp.dev" = {
+          useACMEHost = "jaydenp.dev";
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
+            proxyWebsockets = true;
+          };
+        };
+      };
+    };
+    users.users.nginx.extraGroups = ["acme"];
+
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "jayden.pahukula@gmail.com";
+      certs = {
+        "jaydenp.dev" = {
+          domain = "*.jaydenp.dev";
+          dnsProvider = "cloudflare";
+          credentialsFile = "/run/secrets/cloudflare";
+          dnsPropagationCheck = true;
+        };
       };
     };
 
